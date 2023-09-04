5234d3409b10af5f9d47e7c602e40e74763ca0f8
Hamidreza Bahramian <hamid.bahramian@gmail.com>
2023-09-03 21:19:24 -0400

    completed!

--- app/src/main/java/edu/iu/habahram/secretmessage_completed/EncryptFragment.kt
+++ app/src/main/java/edu/iu/habahram/secretmessage_completed/EncryptFragment.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,26 +14,20 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [WelcomeFragment.newInstance] factory method to
+ * Use the [EncryptFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class WelcomeFragment : Fragment() {
-
-
-    override fun onCreateView(
+class EncryptFragment : Fragment() {
+     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
-        val startButton = view.findViewById<Button>(R.id.start)
-
-        startButton.setOnClickListener {
-            view.findNavController()
-                .navigate(R.id.action_welcomeFragment_to_messageFragment)
-        }
-        return view
-    }
+         // Inflate the layout for this fragment
+         val view = inflater.inflate(R.layout.fragment_encrypt, container, false)
+         val message = EncryptFragmentArgs.fromBundle(requireArguments()).message
+         val encryptedView = view.findViewById<TextView>(R.id.encrypted_message)
+         encryptedView.text = message.reversed()
+         return view }
 
 
 }
--- app/src/main/java/edu/iu/habahram/secretmessage_completed/MessageFragment.kt
+++ app/src/main/java/edu/iu/habahram/secretmessage_completed/MessageFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,14 +22,22 @@ private const val ARG_PARAM2 = "param2"
 class MessageFragment : Fragment() {
 
 
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        //Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+        val nextButton = view.findViewById<Button>(R.id.next)
+        val messageView = view.findViewById<TextView>(R.id.message)
+
+        nextButton.setOnClickListener {
+            val message = messageView.text.toString()
+            val action = MessageFragmentDirections
+                .actionMessageFragmentToEncryptFragment(message)
+            view.findNavController().navigate(action)  }
+        return view
     }
 
 
